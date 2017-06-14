c1388979911c0cedcba6df498d990947db2a43f3
Subhash Saurabh <subhashsaurabh419@gmail.com>
2017-06-14 14:19:01 +0530

    Add files via upload

---
+++ assignment3-starter-code/js/app.js
@@ -0,0 +1,70 @@
+(function(){
+  'use strict';
+
+  angular.module('NarrowItDownApp',[])
+  .controller('NarrowItDownController',NarrowItDownController)
+  .service('MenuSearchService',MenuSearchService)
+  .directive('foundItems',FoundItems);
+
+  function FoundItems(){
+    var ddo = {
+      templateUrl : 'foundItems.html',
+      scope : {
+        found: '<' ,
+        onRemove: '&'
+      }
+    };
+    return ddo;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService','$scope']
+  function NarrowItDownController(MenuSearchService,$scope){
+    var list = this;
+    list.search = function(){
+      if ($scope.searchTerm === ""){
+        list.found = [];
+      }
+      else{
+        MenuSearchService.getMatchedMenuItems($scope.searchTerm)
+        .then(function(response){
+          list.found = response;
+        })
+      }
+    };
+
+    list.removeItem = function(itemIndex){
+      list.found.splice(itemIndex,1);
+    };
+  }
+
+
+
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http){
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm){
+      service.data = [];
+      return $http({
+        method: "GET",
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+      })
+      .then(function(response){
+        for (var i = 0; i < response.data.menu_items.length; i++) {
+          if (response.data.menu_items[i].description.includes(searchTerm)){
+            service.data.push(response.data.menu_items[i])
+          }
+        }
+        return service.data;
+      })
+      .catch(function (error) {
+      console.log(error);
+    })
+    };
+
+  }
+
+
+
+})();
